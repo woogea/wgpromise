03752c73a8972edc6376eeb88aab8e35b04220d6
woogy <rsswoogea@gmail.com>
2018-08-16 11:44:51 +0900

    to async

--- wgpromise.go
+++ wgpromise.go
@@ -1,11 +1,27 @@
 package wgpromise
 
 // Promise struct
+type status int
+
+const (
+	//Pending status is pending
+	Pending = iota
+	//Rejected status is rejected
+	Rejected
+	//Resolved status is resolved
+	Resolved
+)
+
+// Promise is promise
 type Promise struct {
 	resolve func()
 	reject  func()
+	stat    status
 }
 
+func (p *Promise) getStatus() status {
+	return p.stat
+}
 func newPromise(resolve func(), reject func()) *Promise {
 	p := new(Promise)
 	p.reject = reject
@@ -15,10 +31,15 @@ func newPromise(resolve func(), reject func()) *Promise {
 
 // Run promise run
 func (p *Promise) Run(task func() bool) *Promise {
-	if task() {
-		p.resolve()
-	} else {
-		p.reject()
-	}
+	p.stat = Pending
+	go func() {
+		if task() {
+			p.stat = Resolved
+			p.resolve()
+		} else {
+			p.stat = Rejected
+			p.reject()
+		}
+	}()
 	return p
 }
--- wgpromise_test.go
+++ wgpromise_test.go
@@ -35,3 +35,34 @@ func Test_createAndRun(t *testing.T) {
 		return false
 	})
 }
+
+func Test_chain(t *testing.T) {
+	arr := []int{1, 3, 5, 7, 9, 2, 4, 6, 8, 10}
+	p := newPromise(func() { fmt.Println(arr) }, func() { fmt.Println("failed") })
+	p.Run(
+		func() bool {
+			//sorted := make([]int, len(arr))
+			for i := 0; i < len(arr); i++ {
+				minindex := findMinimum(arr[i:len(arr)])
+				arr[i], arr[minindex+i] = swap(arr[i], arr[minindex+i])
+			}
+			return true
+		})
+	for p.stat == Pending {
+	}
+}
+
+func swap(x int, y int) (int, int) {
+	return y, x
+}
+func findMinimum(subarr []int) int {
+	min := subarr[0]
+	minindex := 0
+	for i := 0; i < len(subarr); i++ {
+		if subarr[i] < min {
+			min = subarr[i]
+			minindex = i
+		}
+	}
+	return minindex
+}
